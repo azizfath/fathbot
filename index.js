79c6e1b4a13f00104c062a1d72d20f188b97f03d
azizfath <azizfath35@gmail.com>
2022-10-04 19:14:25 +0700

    penambahan lokasi dan db

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const qr_failed = 'QR tidak dapat dibaca'
 
 const ua = '@m!k0mXv=#neMob!le'
 
+const JSONdb = require('simple-json-db');
+const db = new JSONdb('./dbFathbot.json');
+
 wa.create({
         sessionId: 'FathBot',
         disableSpins: true,
@@ -29,13 +32,78 @@ async function start(client) {
     client.onMessage(async message => {
         const { type, id, from, t, sender, chat, caption, isMedia, mimetype, quotedMsg, body } = message
         const data = caption || body
-        var m = data.split(' ')
+        let m = data.split(' ')
         m[0] = m[0].toLowerCase()
 
+        var img
+        if (isMedia && type === 'image') {
+            try {
+                await db.has(from);
+            } catch (error) {
+
+            }
+            img = await wa.decryptMedia(message)
+            var qr_text, nim = db.get(from);
+            if (img != null) {
+                qr_text = await decode_qr(img)
+                if (qr_text == qr_failed) {
+                    await client.reply(from, qr_failed, id)
+                    return
+                }
+            }
+            if (nim == null) {
+                await client.reply(from, 'Masukan NIM terlebih dahulu', id)
+                return
+            }
+            if (qr_text == null) {
+                await client.reply(from, 'Tidak ada data QR', id)
+                return
+            }
+
+            if (nim != null && qr_text != null && qr_text != qr_failed) {
+                result = await presensi_qr(qr_text, nim)
+                client.reply(from, result, id)
+            }
+        }
+
+
         switch (m[0]) {
-            case '.s':
-            case '.st':
-            case '.status':
+            case '.setnim':
+                if (isNim.test(m[1])) nim = m[1]
+                try {
+                    await db.set(`${from}`, nim)
+                    client.reply(from, `Nim Anda ${nim} telah disimpan untuk nomor Anda`, id)
+                } catch (error) {
+                    console.log(error)
+                }
+                break
+            case '.ceknim':
+                try {
+                    if (await db.has(from)) {
+                        var result = await db.get(from)
+                        client.reply(from, `Nim Anda adalah: ${result}`, id)
+
+                    } else {
+                        client.reply(from, `Anda belum menyimpan NIM`, id)
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+                break
+            case '.hapusnim':
+                try {
+                    if (await db.has(from)) {
+                        var result = await db.get(from)
+                        await db.delete(from)
+                        client.reply(from, `Nim Anda ${result} telah dihapus`, id)
+                    } else {
+                        client.reply(from, `Anda belum menyimpan NIM`, id)
+
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+                break
             case '.':
                 client.reply(from, 'Server Alive', id)
                 break
@@ -43,7 +111,7 @@ async function start(client) {
             case '.a':
             case '.presensi':
             case '.absen':
-                var nim, qr_text
+                var qr_text, nim
                 for (i = 1; i < m.length; i++) {
                     if (m[i] == '-nim' || m[i] == '-n') {
                         tmp = m[i + 1]
@@ -62,7 +130,6 @@ async function start(client) {
                     }
                 }
 
-                var img
                 if (isMedia && type === 'image') {
                     img = await wa.decryptMedia(message)
                 } else if (quotedMsg && quotedMsg.type === 'image') {
@@ -92,9 +159,7 @@ async function start(client) {
                 break
 
             case '.qr':
-            case '.qrcode':
-            case '.decode':
-                var img
+
                 if (isMedia && type === 'image') {
                     img = await wa.decryptMedia(message)
                 } else if (quotedMsg && quotedMsg.type === 'image') {
@@ -105,26 +170,6 @@ async function start(client) {
                 await client.reply(from, qr_text, id)
                 break
 
-            case '.link':
-            case '.jadwal':
-                await client.reply(from, '==== SENIN ====' +
-                    '\n08.40 RisetOp [05.03.06]\n' + jadwal.RISETOP +
-                    '\n\n09.50 BigData T [05.03.06]\n' + jadwal.BIGDATAT1 +
-                    '\n\n11.00 ANSI [05.03.06]\n' + jadwal.ANSI1 +
-                    '\n\n\n==== SELASA ====' +
-                    '\n08.40 PBD(3) T [05.04.02]\n' + jadwal.PBD3T +
-                    '\n\n09.50 AI [04.02.01]\n' + jadwal.AI +
-                    '\n\n11.00 Kecakap [04.03.03]\n' + jadwal.KECAKAP +
-                    '\n\n13.00 ANSI [04.03.03]\n' + jadwal.ANSI2 +
-                    '\n\n14.10 P.WEB T [04.03.03]\n' + jadwal.PWEBT +
-                    '\n\n\n==== RABU ====' +
-                    '\n08.40 Aljabar [05.03.01]\n' + jadwal.ALJABAR +
-                    '\n\n09.50 P.WEB P [L 2.4.3]\n' + jadwal.PWEBP +
-                    '\n\n11.00 BigData T [05.03.08]\n' + jadwal.BIGDATAT2 +
-                    '\n\n11.00 BigData P [L 7.3.2]\n' + jadwal.BIGDATAP +
-                    '\n\n\n==== KAMIS ====' +
-                    '\n07.30 PBD(3) P [L 2.4.2]\n' + jadwal.PBD3P, id)
-                break
             default:
                 break
         }
@@ -132,7 +177,7 @@ async function start(client) {
 }
 
 async function scanQRcode(path) {
-    var img = await Jimp.read(path)
+    let img = await Jimp.read(path)
     qrcode = new QRreader()
     qrcode.callback = function(err, value) {
         if (err) {
@@ -190,15 +235,16 @@ async function presensi_qr(data, nim) {
                 'user-agent': ua,
             },
             json: {
-                data: `${data};${nim}`
+                data: `${data};${nim}`,
+                location: `Amikom`
             }
         }).json()
         return nim + ': Berhasil Presensi'
 
     } catch (error) {
-        console.log(error.response);
+        //console.log(error.response);
         if (error.response.statusCode == 400) {
-            return 'Response : 400 Bad Request'
+            return 'QR Expired'
         }
         if (error.response.statusCode == 422) {
             return nim + ': Sudah Presensi'
